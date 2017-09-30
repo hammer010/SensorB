1d20da1b54d98b87f3da1e57ea75fd9a847e280c
hammer010 <32393979+hammer010@users.noreply.github.com>
2017-09-30 16:15:58 +0200

    Update test_beta.py

--- test_beta.py
+++ test_beta.py
@@ -16,23 +16,24 @@ gpio.init()
 print("Start program")
 i = 0
 
-while i < 10 :
-    # read data using pin 14
-    instance = dht22.DHT22(pin=PIN2)
-            
-        while True:
+
+while True:
             result = instance.read()
             if result.is_valid():
-        
-            list_temp = []
-            list_temp.append(result.temperature)
-            list_hum = []
-            list_hum.append(result.humidity)
-            print list_temp
-            print list_hum
-            i = i + 1
-            print i
-            time.sleep(5)
+
+                while i < 10 :
+                    # read data using pin 14
+                    instance = dht22.DHT22(pin=PIN2)
+                    
+                    list_temp = []
+                    list_temp.append(result.temperature)
+                    list_hum = []
+                    list_hum.append(result.humidity)
+                    print list_temp
+                    print list_hum
+                    i = i + 1
+                    print i
+                    time.sleep(5)
 
 
        #print("Horodatage: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
