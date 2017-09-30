6556044b9e24d484a6e0a6bd80a0b4f36e979c88
hammer010 <32393979+hammer010@users.noreply.github.com>
2017-09-30 16:46:11 +0200

    Update test_beta.py

--- test_beta.py
+++ test_beta.py
@@ -33,8 +33,9 @@ while 1 :
             print i
             time.sleep(5)
             
-            if i == 10 : 
-                break
+if i == 10 : 
+    break
+    
 print("etat des listes")
 print list_temp
 print list_hum
