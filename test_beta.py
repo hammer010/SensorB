b6a6e5609a830a1e4776230d80bc2aaf9d70d44a
hammer010 <32393979+hammer010@users.noreply.github.com>
2017-09-30 16:24:05 +0200

    Update test_beta.py

--- test_beta.py
+++ test_beta.py
@@ -1,49 +1,38 @@
 from pyA20.gpio import gpio
-from pyA20.gpio import port
-
-#import RPi.GPIO as GPIO
-import dht22
-import time
-import datetime
-
-# initialize GPIO
-#gpio.setwarnings(False)
-#gpio.setmode(GPIO.BCM)
-PIN2 = port.PA6
-gpio.init()
-#gpio.cleanup()
-
-print("Start program")
-i = 0
-
-
-while True:
-            result = instance.read()
-            if result.is_valid():
-
-                while i < 10 :
-                    # read data using pin 14
-                    instance = dht22.DHT22(pin=PIN2)
-                    
-                    list_temp = []
-                    list_temp.append(result.temperature)
-                    list_hum = []
-                    list_hum.append(result.humidity)
-                    print list_temp
-                    print list_hum
-                    i = i + 1
-                    print i
-                    time.sleep(5)
-
-
-       #print("Horodatage: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-       #print("Temperature: %.2f C" % result.temperature)
-       #print("Humidity: %.2f %%" % result.humidity)
-        
-        #fichier = open("data.txt", "w")
-        #fichier.write("Horodatage: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        #fichier.write("\nTemperature: %.2f C" % result.temperature)
-        #fichier.write("\nHumidite: %.2f %%" % result.humidity)
-        #fichier.close()
-
-time.sleep(1)
+ from pyA20.gpio import port
+ 
+ #import RPi.GPIO as GPIO
+ import dht22
+ import time
+ import datetime
+ 
+ # initialize GPIO
+ #gpio.setwarnings(False)
+ #gpio.setmode(GPIO.BCM)
+ PIN2 = port.PA6
+ gpio.init()
+ #gpio.cleanup()
+ 
+ 
+ # read data using pin 14
+ instance = dht22.DHT22(pin=PIN2)
+ 
+ print("Start program")
+ i = 0
+ 
+ while True:
+     result = instance.read()
+     if result.is_valid():
+ 
+         
+         while i < 10 :
+             list_temp = []
+             list_temp.append(result.temperature)
+             list_hum = []
+             list_hum.append(result.humidity)
+             print list_temp
+             print list_hum
+             i = i +1
+             i = i + 1
+             print i
+             time.sleep(5)
