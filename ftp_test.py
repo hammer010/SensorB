cddc9f4b1286d08b095e57a182e204da65a17638
hammer010 <32393979+hammer010@users.noreply.github.com>
2017-10-19 22:59:42 +0200

    Update ftp_test.py

--- ftp_test.py
+++ ftp_test.py
@@ -6,8 +6,13 @@ print "Etat : ",etat
 
 ftp.cwd("/home/pi/Bureau")
 
+# Ouverture Fichier
+fichier = "/home/orangepi/develop/build/test.txt"
+file = open(fichier,'rb')
 
-
+# Envoi Fichier
+ftp.storbinary('STOR '+'test.csv', file)
+ftp.retrlines('LIST')
 print ftp.dir()
 
 ftp.close()
