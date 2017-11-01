6adf9295cdee3689ee9a73ac847c8cda464eab5b
hammer010 <32393979+hammer010@users.noreply.github.com>
2017-11-01 22:39:00 +0100

    Update Python_ON.py

--- Python_ON.py
+++ Python_ON.py
@@ -22,46 +22,47 @@ gpio.init()
 
 def average(list): 
     if (list!= []): return float(sum(list)) / len(list)
+while true
+    # read data using pin 14
+    instance = dht22.DHT22(pin=PIN2)
 
-# read data using pin 14
-instance = dht22.DHT22(pin=PIN2)
+    i = 0
+    list_temp = []
+    list_hum = []
 
-i = 0
-list_temp = []
-list_hum = []
-
-while True :
-    result = instance.read()
-    if result.is_valid():
+    while true :
+        result = instance.read()
+        if result.is_valid():
         
         list_temp.append(result.temperature)
         list_hum.append(result.humidity)
         i = i + 1
         print("Mesure N*" + str(i) + " effectuee")
-        time.sleep(2)
+        time.sleep(6)
     if i == 10 : 
         break
 
-print("Horodatage: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) 
-print("Temperature: " + str(average(list_temp)) + " C")
-print("Humidite: " + str(average(list_hum)) + " %")
+    print("Horodatage: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) 
+    print("Temperature: " + str(average(list_temp)) + " C")
+    print("Humidite: " + str(average(list_hum)) + " %")
 
 # Creation fichier de resultats
-fichier = open("sensorA.txt", "w")
-fichier.write(datetime.datetime.now().strftime("%H:%M"))
-fichier.write("\n" + str(average(list_temp)))
-fichier.write("\n" + str(average(list_hum)))
-fichier.close()
+    fichier = open("sensorA.txt", "w")
+    fichier.write(datetime.datetime.now().strftime("%H:%M"))
+    fichier.write("\n" + str(average(list_temp)))
+    fichier.write("\n" + str(average(list_hum)))
+    fichier.close()
 
 # Envoi resultats vers serveur FTP
-ftp.cwd("/var/www/html")
-fichier = "/home/orangepi/develop/DHT22-Python-library-Orange-PI/sensorA.txt"
-file = open(fichier,'rb')
-ftp.storbinary('STOR '+'sensorA.txt', file)
-ftp.retrlines('LIST')
-print ftp.dir()
-ftp.close()
+    ftp.cwd("/var/www/html")
+    fichier = "/home/orangepi/develop/DHT22-Python-library-Orange-PI/sensorA.txt"
+    file = open(fichier,'rb')
+    ftp.storbinary('STOR '+'sensorA.txt', file)
+    ftp.retrlines('LIST')
+    print ftp.dir()
+    ftp.close()
 
 # Fin du programme
 print("Sauvegarde des resultats effectuee")
-print("Fin programme")
+print("Attente 5 min")
+time.sleep(300)
